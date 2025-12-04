511696d75e754aca8d177bd40528f3598cae30c0
Clark Wang <clarktwang@gmail.com>
2025-12-04 11:21:37 -0800

    to-do list, also I just realized I just did what pandas does to data --- I mean we have tools I just don't know we have them - I'm trying to build a car with a hammer and duct tape

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import plotly.graph_objects as go
 from data.cleanedData import cityPopulationDict
 
+'''
+To-do: need a way to actually draw the lines, maybe using pygame for that - I think that pygame has more control over the figures I create and could be a better library
+for animation in general. I think that plotly works great for quick data representation, but I don't think I will have the needed animations with it. I could use it to 
+quickly get the plots I need then use pygame to implement the acutal animation behind forming the interstates
+
+- get the actual lines to generate in an animation
+- figure out how to actually use plotly
+'''
+
 fig = go.Figure(go.Scattermap(
         lat=['45.5017'],
         lon=['-73.5673'],
